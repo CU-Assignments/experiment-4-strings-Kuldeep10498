b32f410326b330857a2c5f1db220e67afbfefd99
Kuldeep10498 <139319352+Kuldeep10498@users.noreply.github.com>
2025-02-01 22:07:22 +0530

    Create FirstOccurence.java

---
+++ FirstOccurence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int haylength = haystack.length();
+        int needlelength = needle.length();
+        if (haylength < needlelength)
+            return -1;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j))
+                j++;
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(solution.strStr(haystack, needle));
+        
+        haystack = "aaaaa";
+        needle = "bba";
+        System.out.println(solution.strStr(haystack, needle));
+    }
+}
